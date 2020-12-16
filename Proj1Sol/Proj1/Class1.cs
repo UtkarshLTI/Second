7aa5685046047dfdd0f7e7faaf2069fad70994f4
utkarsh gami <Utkarsh.Gami@lntinfotech.com>
2020-12-16 10:41:08 +0530

    added 2

---
+++ Proj1Sol/Proj1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj1
+{
+    partial class Class1
+    {
+        void Print1()
+        { }
+    }
+}
